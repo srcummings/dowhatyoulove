452fe214c8aea1a7809f0cd71f702d7552aaa37f
Russ Cummings <russ.cummings@outlook.com>
2019-06-27 21:37:58 -0500

    changed welcome message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    My Laravel
+                    Welcome
                 </div>
 
                 <div class="links">
